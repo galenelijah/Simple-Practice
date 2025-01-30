ca5cbe719d3c9437fc9a59325318c709342aeafc
Galen Elijah Sabequil <sgalenelijah@gmail.com>
2025-01-30 20:15:19 +0800

    Practice w/ random JS aspects

---
+++ something.js
@@ -0,0 +1,16 @@
+
+
+
+const person = {
+    fullName: "Galen Elijah Sabequil",
+    age: 30,
+    isStudent: true,
+    hobbies: ['karate', 'basketball', 'video games'],
+    address: {
+        street: "124 Conch St.",
+        city: "Lapu-Lapu City",
+        country: "Philippines"
+    }
+}
+
+console.log(person);
